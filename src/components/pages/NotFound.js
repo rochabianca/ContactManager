6f3aed6adc1dd11e1d6ad472730d2a114a3334ea
Bianca <bianca.rocha1711@gmail.com>
2018-09-02 14:22:25 -0300

    create the Not Found Page

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      <h1 className="display-4">
+        <span className="text-danger">404</span> Page not Found
+      </h1>
+      <p className="lead">Sorry, the page you are looking for is not here.</p>
+    </div>
+  );
+};
