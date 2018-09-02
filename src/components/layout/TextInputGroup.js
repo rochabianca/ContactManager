1f6562d1055b9a520d2c36ba4a16405c05cb1bfb
Bianca <bianca.rocha1711@gmail.com>
2018-09-02 15:46:00 -0300

    Add contact now makes a request for the database and a minor bug fix

--- src/components/layout/TextInputGroup.js
+++ src/components/layout/TextInputGroup.js
@@ -1,5 +1,5 @@
 import React from "react";
-import PropTypes from "prop-types";
+import propTypes from "prop-types";
 import classnames from "classnames";
 
 const TextInputGroup = ({
@@ -29,14 +29,14 @@ const TextInputGroup = ({
   );
 };
 
-TextInputGroup.PropTypes = {
-  label: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  placeholder: PropTypes.string.isRequired,
-  value: PropTypes.string.isRequired,
-  type: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
-  error: PropTypes.string
+TextInputGroup.propTypes = {
+  label: propTypes.string.isRequired,
+  name: propTypes.string.isRequired,
+  placeholder: propTypes.string.isRequired,
+  value: propTypes.string.isRequired,
+  type: propTypes.string.isRequired,
+  onChange: propTypes.func.isRequired,
+  error: propTypes.string
 };
 
 TextInputGroup.defaultProps = {
