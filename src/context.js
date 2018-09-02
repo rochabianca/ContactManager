3eec1837a5b2f3138db3a0fd601bac43fb1222e9
Bianca <bianca.rocha1711@gmail.com>
2018-09-02 15:29:13 -0300

    set up fake data from json placeholder

--- src/context.js
+++ src/context.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 const Context = React.createContext();
 
@@ -24,28 +25,17 @@ const reducer = (state, action) => {
 
 export class Provider extends Component {
   state = {
-    contacts: [
-      {
-        id: 1,
-        name: "John Doe",
-        email: "jdoe@gmail.com",
-        phone: "555-555-5555"
-      },
-      {
-        id: 2,
-        name: "Karen Smith",
-        email: "karen@gmail.com",
-        phone: "555-555-5565"
-      },
-      {
-        id: 3,
-        name: "Kara Williams",
-        email: "kara@gmail.com",
-        phone: "555-555-5565"
-      }
-    ],
+    contacts: [],
     dispatch: action => this.setState(state => reducer(state, action))
   };
+
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/users").then(res =>
+      this.setState({
+        contacts: res.data
+      })
+    );
+  }
 
   render() {
     return (
